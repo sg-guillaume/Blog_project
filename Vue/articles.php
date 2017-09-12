460b53c1addef7f3d77d8bd8e6c865db96f635c4
guillaume <sg-guillaume@orange.fr>
2017-09-12 14:19:37 +0200

    put a basic material design on articles view

--- Vue/articles.php
+++ Vue/articles.php
@@ -2,24 +2,67 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<title>Accueil</title>
+
+	<!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+	
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 </head>
 <body>
-	<h1>Blog de jean Forteroche</h1>
+
+	<header>
+		
+		<nav>
+		  <div class="nav-wrapper">
+		    <a href="#" class="brand-logo">Jean Forteroche</a>
+		    <ul id="nav-mobile" class="right hide-on-med-and-down">
+		      <li><a href="#">Biographie</a></li>
+		      <li><a href="#">Chapitres</a></li>
+		      <li><a href="#">Contact</a></li>
+		      <li><a href="#">Inscription</a></li>
+		    </ul>
+		  </div>
+		</nav>
+		      
+	</header>
+	<div class="container center-align">
+		<h2>Tous les chapitres</h2>
+	</div>
+		
+
 
 	<main>
-		<section>
+		<section class="section">
+			<div class="row">
 			<?php foreach ($articles as $article): ?>
-			 <h3><?= $article->getTitle(); ?></h3>
-			 <p>
-			 	<?= $article->getAuthor(); ?> le <span><?= $article->getCreationDate(); ?></span>
-			 </p>
-			 <p>
-			 	<?= $article->getContent(); ?>
-			 </p>
+				
+				  <div class="col s12 m4">
+				    <ul class="collapsible hoverable" data-collapsible="accordion">
+				    	<li>
+					      <div class="collapsible-header">
+					      	<div class="container center-align">
+					        	<h5><?= $article->getTitle(); ?></h5>
+					        	<span><?= $article->getAuthor() . ' le ' . $article->getCreationDate(); ?></span>
+					        </div>
+					      </div>
+					      
+					      <div class="collapsible-body">
+					        <p><?= $article->getContent(); ?></p>
+					      </div>
+					    </li>
+				    </ul>
+				  </div>
+										
 			<?php endforeach; ?>
+			</div>
 		</section>
 	</main>
+	
 </body>
 </html>
